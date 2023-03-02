4e92265a4a415c84336752d782bd73bb36961e78
Lorrayne <lorrayne.au@gmail.com>
2023-03-02 17:46:25 -0300

    requisicoes-http

--- src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -1,4 +1,7 @@
+import { Pensamento } from './../pensamento';
 import { Component, OnInit } from '@angular/core';
+import { PensamentoService } from '../pensamento.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-criar-pensamento',
@@ -6,17 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./criar-pensamento.component.css'],
 })
 export class CriarPensamentoComponent implements OnInit {
-  pensamento = {
-    id: `1`,
-    conteudo: `Aprendendo Angular`,
-    autoria: `Dev`,
+  pensamento: Pensamento = {
+    conteudo: ``,
+    autoria: ``,
     modelo: `modelo1`,
   };
 
-  constructor() {}
+  constructor(private service: PensamentoService, private router: Router) {}
 
   ngOnInit(): void {}
 
-  criarPensamento() {}
-  cancelar() {}
+  criarPensamento() {
+    this.service.criar(this.pensamento).subscribe(() => {
+      this.router.navigate(['/listarPensamento']);
+    });
+  }
+  cancelar() {
+    this.router.navigate(['/listarPensamento']);
+  }
 }
